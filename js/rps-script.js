b31366af68111177a47cdae031242f233c132d52
Miguel <macgarcia2002@gmail.com>
2022-06-05 17:47:38 -0400

    Create function for computer's move

---
+++ js/rps-script.js
@@ -0,0 +1,22 @@
+//function computerPlay() {
+//    let rock = 0;
+//    let paper = 1;
+//    let scissors = 2;
+//    return Math.floor(Math.random(3))
+//}
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min +1) + min);
+}
+
+let move = randomNumber(1, 3);
+
+function computerPlay(move) {
+    if (move == 1) {
+        return "rock";
+    } else if (move == 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+

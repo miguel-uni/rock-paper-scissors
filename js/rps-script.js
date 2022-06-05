f6cff92601f66bbc52dc96c1d2a3eeaefcf1bd3c
Miguel <macgarcia2002@gmail.com>
2022-06-05 18:55:50 -0400

    Create function to declare the move of the player

--- js/rps-script.js
+++ js/rps-script.js
@@ -1,15 +1,11 @@
-//function computerPlay() {
-//    let rock = 0;
-//    let paper = 1;
-//    let scissors = 2;
-//    return Math.floor(Math.random(3))
-//}
+//random number generator to randomize move of computer between 1 and 3
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min +1) + min);
 }
 
 let move = randomNumber(1, 3);
 
+//function to declare move of computer
 function computerPlay(move) {
     if (move == 1) {
         return "rock";
@@ -22,8 +18,20 @@ function computerPlay(move) {
 
 let computerSelection = computerPlay(move);
 
-let playerSelection = prompt("What is your move? (rock, paper, scissors)");
+//prompt and function for the player to pick a move
+let playerMove = prompt("What is your move? (rock, paper or scissors)");
 
-function firstRound(computerSelection, playerSelection) {
-
+function playerPlay(playerMove) {
+    if (playerMove.toLowerCase() == "rock") {
+        return 1;
+    } else if (playerMove.toLowerCase() == "paper") {
+        return 2;
+    } else if (playerMove.toLowerCase() == "scissors") {
+        return 3;
+    } else {
+        return "You made a mistake!";
+    }
 }
+
+let playerSelection = playerPlay(playerMove);
+

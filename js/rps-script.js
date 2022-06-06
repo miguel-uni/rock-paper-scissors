1244f3ca5739d25bc5d9b45d7683c78c64031df5
Miguel <macgarcia2002@gmail.com>
2022-06-05 22:15:17 -0400

    Move variables inside functions and Start to write code for a game of 5 rounds

--- js/rps-script.js
+++ js/rps-script.js
@@ -1,12 +1,14 @@
 //random number generator to randomize move of computer between 1 and 3
+
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min +1) + min);
 }
 
-let move = randomNumber(1, 3);
-
 //function to declare move of computer
-function computerPlay(move) {
+
+function computerPlay(randomNumber) {
+    let move = randomNumber(1, 3);
+
     if (move == 1) {
         return "rock";
     } else if (move == 2) {
@@ -16,12 +18,12 @@ function computerPlay(move) {
     }
 }
 
-const computerSelection = computerPlay(move);
+//prompt and function for the player to pick a move and it's not case sensitive
 
-//prompt and function for the player to pick a move
-let playerMove = prompt("What is your move? (rock, paper, scissors)");
 
-function playerPlay(playerMove) {
+function playerPlay() {
+    let playerMove = prompt("What is your move? (rock, paper, scissors)");
+    
     if (playerMove.toLowerCase() == "rock") {
         return "rock";
     } else if (playerMove.toLowerCase() == "paper") {
@@ -33,24 +35,42 @@ function playerPlay(playerMove) {
     }
 }
 
-const playerSelection = playerPlay(playerMove);
 
 //fuction to select the winner of the round 
-function playRound(playerSelection, computerSelection) {
+
+function playRound(computerPlay, playerPlay) {
+    const computerSelection = computerPlay(randomNumber);
+    const playerSelection = playerPlay();
+    
+    console.log(computerSelection);
+    console.log(playerSelection);
+
+    let loser = `You lose! ${computerSelection} beats ${playerSelection}`;
+    const winner = "You win this round!";
+    const tie = "We have a tie!";
+
     if (computerSelection == playerSelection) {
-        return "We have a tie!";
+        return tie;
     } else if ((computerSelection == "rock") && (playerSelection == "scissors")) {
-        return "You lose! Rock beats Scissors";
+        return loser;
     } else if ((computerSelection == "paper") && (playerSelection == "rock")) {
-        return "You lose! Paper beats Rock";
+        return loser;
     } else if ((computerSelection == "scissors") && (playerSelection == "paper")) {
-        return "You lose! Scissors beats Paper";
+        return loser;
     } else {
-        return "You win this round!";
+        return winner;
     }
-  }
+}
+
+//function to start a game of 5 rounds 
+
+function game(playRound, playerPlay) {
+    for (let i = 0; i < 5; i++) {
 
 
-console.log(computerSelection);
-console.log(playerSelection);
-console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+console.log(playRound(computerPlay, playerPlay));
+
+//console.log(game(playRound(playerSelection, computerSelection)));
